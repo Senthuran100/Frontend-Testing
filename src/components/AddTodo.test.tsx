9ee69088e4f4dce2954447857460d02d34c69c6e
ext_smanoharan <senthuran.manoharan@gmail.com>
2023-10-08 12:22:59 +1100

    Add unit testing for AddTodo component.

--- src/components/AddTodo.test.tsx
+++ src/components/AddTodo.test.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import {  screen } from '@testing-library/react';
+import {  screen, waitFor } from '@testing-library/react';
 import {AddTodo} from './AddTodo';
 import { renderWithProviders } from '../utils/test-utils';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+test('renders Add Todo component and do unit testing', async () => {
  
   renderWithProviders(<AddTodo saveTodo={()=>{}}/>)
 
   const dataElement = screen.getByText('Add Todo');
   expect(dataElement).toBeInTheDocument();
-  
+  userEvent.type(screen.getByPlaceholderText("Title"), "Title")
+  userEvent.type(screen.getByPlaceholderText("Description"), "Description")
+  await waitFor(() => {
+    expect(screen.getByPlaceholderText("Title")).toHaveValue("Title");
+    expect(screen.getByPlaceholderText("Description")).toHaveValue("Description");
+ });
 });
